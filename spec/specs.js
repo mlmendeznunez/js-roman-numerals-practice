d1c9b38a789810ec146b085e346c5586ea75304d
Andrea Padgett <student@epicodus.com>
2015-09-16 14:58:55 -0700

    improved html and css

--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,3 @@
-// describe('romanNumerals', function() {
-//   it("returns 1 for input 1", function() {
-//     expect(howLarge("1")).to.equal(1);
-//   });
-//
-//   it("returns 4 for input 1000", function() {
-//     expect(howLarge("1000")).to.equal(4);
-//   });
-//
-//   it("returns error message for input greater than 4 digits", function() {
-//     expect(howLarge(10000)).to.equal("Oops! You entered a number greater than 3,999. Try again!");
-//   });
-// });
-
 
 describe('howLarge', function(){
   it("returns error message for input greater than 3999", function(){
