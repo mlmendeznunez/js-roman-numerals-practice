f3269905289f6cb6c42baa3338bbf5937f6ce5fe
perry eising & morgan lutz <student@epicodus.com>
2015-09-16 11:56:30 -0700

    Add functionality to handle numbers 0-100 and implement function chain

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,13 @@
 describe('romanNumerals', function() {
-  it("returns I for input 1", function() {
-    expect(romanNumerals(1)).to.equal("I");
+  it("returns 1 for input 1", function() {
+    expect(howLarge("1")).to.equal(1);
+  });
+
+  it("returns 4 for input 1000", function() {
+    expect(howLarge("1000")).to.equal(4);
+  });
+
+  it("returns error message for input greater than 4 digits", function() {
+    expect(howLarge("10000")).to.equal("Oops! You entered a number greater than 3,999. Try again!");
   });
 });
